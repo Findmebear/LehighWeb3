e10bcce4754b5c898cb67d201f026c59486b9113
Steven Nazaroff <stn223@lehigh.edu>
2022-06-08 14:47:07 -0400

    created a smart contract

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,22 +1,46 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-import "hardhat/console.sol";
+pragma solidity ^0.5.0;
 
 contract Greeter {
-    string private greeting;
+  uint public postCount = 0;
+  string public name = "DTube";
+  mapping(uint => Post) public posts;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
-    }
+  struct Post {
+    uint id;
+    string text;
+    string hash;
+    string title;
+    address author;
+  }
 
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
+  event PostCreated(
+    uint id,
+    string text,
+    string hash,
+    string title,
+    address author
+  );
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
-    }
+  constructor() public {
+  }
+
+  function createPost(string memory _videoHash, string memory _title, string memory _text) public {
+    // Make sure the text exists and smaller than 500 
+    require(bytes(_text).length > 0 && bytes(_text).length < 501);
+    // Make sure the video hash exists
+    require(bytes(_videoHash).length > 0);
+    // Make sure video title exists
+    require(bytes(_title).length > 0);
+    // Make sure uploader address exists
+    require(msg.sender!=address(0));
+
+    // Increment video id
+    postCount++;
+
+    // Add video to the contract
+    posts[postCount] = Post(postCount, _text, _videoHash, _title, msg.sender);
+    // Trigger an event
+    emit PostCreated(id, _text, hash, title, author); 
+    (videoCount, _videoHash, _title, msg.sender);
+  }
 }
