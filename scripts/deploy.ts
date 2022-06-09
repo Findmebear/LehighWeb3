d0773cd81f4b90ac308acf4de25a0acec8b177c3
Artem Maryanskyy <arm320@lehigh.edu>
2022-06-09 10:11:18 -0400

    deploy.ts changed

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const CreatePost = await ethers.getContractFactory("CreatePost");
+  const createPost = await CreatePost.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  await createPost.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("CreatePost deployed to:", createPost.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
