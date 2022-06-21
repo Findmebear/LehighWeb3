2db4feb635dfb940a0cf5dd95654c2f05d0b98ef
Findmebear <arm320@lehigh.edu>
2022-06-21 17:51:22 -0400

    moving connectWallet and essential constants to the wrapper from Navbar to make it accessible to other components too

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import { useContext } from "react";
 import * as ReactDOM from "react-dom";
+import { TransactionContext } from "../../context/TransactionContext";
 import "./navbar.css";
 
 
@@ -7,61 +9,11 @@ const Navbar = () => {
   /*
   * Just a state variable we use to store our user's public wallet.
   */
-  const [currentAccount, setCurrentAccount] = useState("");
 
-  const checkIfWalletIsConnected = async() => {
-    try {
-    /*
-    * First make sure we have access to window.ethereum
-    */
-      const { ethereum } = window;
-      setCurrentAccount("Sign In");
-      if (!ethereum) {
-        console.log("Make sure you have metamask!");
-      } else {
-        console.log("We have the ethereum object", ethereum);
-      }
-      const accounts = await ethereum.request({ method: "eth_accounts" });
-
-        if (accounts.length !== 0) {
-          const account = accounts[0];
-          console.log("Found an authorized account:", account);
-          setCurrentAccount(account)
-        } else {
-          console.log("No authorized account found")
-        }
-      } catch  (error) {
-        console.log(error);
-  }
-}
+  const {currentAccount, connectWallet} = useContext(TransactionContext);
 
 
-const connectWallet = async () => {
-  try {
-    const { ethereum } = window;
-
-    if (!ethereum) {
-      alert("Get MetaMask!");
-      return;
-    }
-
-    const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-
-    console.log("Connected", accounts[0]);
-    setCurrentAccount(accounts[0]);
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-
-
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, [])
-
-
-  return (
+return (
   <div class="leftnav">
     <button > WEB3 App </button>
     <button >Profile</button>
@@ -72,9 +24,6 @@ const connectWallet = async () => {
     
 
   </div>
-
-
-
   );
 };
 
--- src/context/TransactionContext.jsx
+++ src/context/TransactionContext.jsx
@@ -20,8 +20,57 @@ const getEthereumContract = () => {
 }
 
 export const TransactionsProvider = ({ children }) =>  {
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    const connectWallet = async () => {
+        try {
+          const { ethereum } = window;
+      
+          if (!ethereum) {
+            alert("Get MetaMask!");
+            return;
+          }
+      
+          const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      
+          console.log("Connected", accounts[0]);
+          setCurrentAccount(accounts[0]);
+        } catch (error) {
+          console.log(error)
+        }
+    }
+    const checkIfWalletIsConnected = async() => {
+        try {
+        /*
+        * First make sure we have access to window.ethereum
+        */
+        const { ethereum } = window;
+        setCurrentAccount("Sign In");
+        if (!ethereum) {
+            console.log("Make sure you have metamask!");
+        } else {
+            console.log("We have the ethereum object", ethereum);
+        }
+        const accounts = await ethereum.request({ method: "eth_accounts" });
+
+            if (accounts.length !== 0) {
+            const account = accounts[0];
+            console.log("Found an authorized account:", account);
+            setCurrentAccount(account)
+            } else {
+            console.log("No authorized account found")
+            }
+        } catch  (error) {
+            console.log(error);
+    }
+    }
+
+    useEffect(() => {
+    checkIfWalletIsConnected();
+    }, [])
+
     return (
-        <TransactionContext.Provider value={{value: "test"}}>
+        <TransactionContext.Provider value={{connectWallet, currentAccount}}>
             {children}
         </TransactionContext.Provider>
     );
