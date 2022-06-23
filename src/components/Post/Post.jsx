a2e4208b9a3baeae2d66e970f3fa9d09a6feb470
Edward Mon <elm224@lehigh.edu>
2022-06-23 13:48:48 -0400

    title added

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import { useContext } from "react";
+import "./post.css";
+const Post = ()=> {
+
+    return(
+        <div>
+            <h1> Create a post!</h1>
+            <form>
+                <input type = "file" name = "upload" accept = "image/*" />
+                <input type="text" name="title" id = "title" placeholder = "Enter your title here" />
+                <input type="text" name="text" id = "text" placeholder = "Enter your text here" />
+            </form>
+            <button> Submit
+            </button>
+        </div>
+
+
+    )
+
+};
+export default Post;
