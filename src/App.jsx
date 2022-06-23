ea3ec4dd1ec2b2322d6f484dfa76a0fa45750dd0
Edward Mon <elm224@lehigh.edu>
2022-06-23 13:35:55 -0400

    simple UI for posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar/Navbar"
-
+import Post from "./components/Post/Post"
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+
+      <div>
+        <Navbar />
+        <Post/>
+      </div>
+      
+    
+
   )
 }
 
