fb35fc65b968909b040474e0c055c43c27b26561
ziw224 <ziw224@lehigh.edu>
2022-06-28 14:42:22 -0400

    change index.js file and update database.sql file

---
+++ server/database.sql
@@ -0,0 +1,13 @@
+CREATE TABLE A_USER(
+    user_id VARCHAR(2083),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    description VARCHAR(65535),
+    CONSTRAINT A_USER_PK PRIMARY KEY(user_id)
+);
+
+insert into a_user (user_id, first_name, last_name, description) values ('0x1fa338050cfe4b141df8e16f7289ef96a65d2220', 'Dun', 'Behling', 'null');
+insert into a_user (user_id, first_name, last_name, description) values ('0x10a387cd1641775244cbc2ae2b48710dc80e4fab', 'Rogers', 'Shery', 'null');
+insert into a_user (user_id, first_name, last_name, description) values ('0xb68653b037f453a34fdf4a932493708e34ed7f80', 'Thorny', 'Prendeguest', 'null');
+insert into a_user (user_id, first_name, last_name, description) values ('0xcae3b8bbda9a317038563455aabe1294ace707d9', 'Carlos', 'Ham', 'null');
+insert into a_user (user_id, first_name, last_name, description) values ('0x10c49efa9ce8d91bdac8ec5e0052f0b1b4fbe833', 'Latrina', 'Paddefield', 'null');
--- server/index.js
+++ server/index.js
@@ -1,31 +1,163 @@
-import express from 'express';
+import express, { response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { createRequire } from 'module';
+import { request } from 'http';
 const require = createRequire(import.meta.url);
 
 const { Pool } = require('pg') //postgreSQL Pool library
 require('dotenv').config()
-const pool = new Pool({
+const devConfig = {
     user: 'wbejlafffgmfxp',
     host: 'ec2-3-228-235-79.compute-1.amazonaws.com',
     database: 'daega0ttst88l3',
     password: process.env.DB_PASSWORD,
     port: 5432,
     ssl: { rejectUnauthorized: false }
-})
+};
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL //heroku addons
+};
+const pool = new Pool(
+    process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 const port = process.env.PORT || 3000;
-
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extend: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extend: true}))
 app.use(cors());
 
+//ROUTES//
+
 app.get('/', (request, response) => {
     response.json({ info: 'Backend Web3 Development Database'})
   });
+
+//create a user
+app.post('/users', async (request, response) =>{
+    try {
+        const { user_id, first_name, last_name, description} = request.body;
+        const newUser = await pool.query(
+            'INSERT INTO a_user (user_id, first_name, last_name, description) VALUES ($1, $2 , $3, $4) RETURNING *',
+             [user_id, first_name, last_name, description]           
+             );
+        response.json(newUser.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//get all users
+app.get('/users', async (request, response) =>{
+    try {
+        const getUsers = await pool.query('SELECT * FROM a_user');
+        response.json(getUsers.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//get an user
+app.get('/users/:user_id', async (request, response) =>{
+    try {
+        const { user_id } = request.params;
+        const user = await pool.query(
+            'SELECT * FROM a_user WHERE user_id = $1', 
+            [user_id]
+        );
+        response.json(user.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+//update an user's first name
+app.put('/users/:user_id', async (request, response) =>{
+    try {
+        let body = [];
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+        // at this point, `body` has the entire request body stored in it as a string
+        });
+        //update first_name
+        const { user_id } = request.params;
+        const { first_name } = request.body;
+        const updateFirstName = await pool.query(
+            'UPDATE a_user SET first_name = $1 WHERE user_id = $2', 
+            [first_name, user_id]
+        );
+        response.json("User's first name was updated!");
+
+        //update last_name
+        const { last_name } = request.body;
+        const updateLastName = await pool.query(
+            'UPDATE a_user SET last_name = $1 WHERE user_id = $2', 
+            [last_name, user_id]
+        );
+        response.json("User's last name was updated!");
+
+            // case 3: const {description} = request.body;
+            //         const updateDescription = await pool.query(
+            //             'UPDATE a_user SET description = $1 WHERE user_id = $2', 
+            //             [description, user_id]
+            //         );
+            //         response.json("User description was updated!");  
+            //         break;              
+        
+        
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// //update an user's last name
+// app.put('/users/:user_id', async (request, response) =>{
+//     try {
+//         const { user_id } = request.params;
+//         const {last_name} = request.body;
+        // const updateUser = await pool.query(
+        //     'UPDATE a_user SET last_name = $1 WHERE user_id = $2', 
+        //     [last_name, user_id]
+        // );
+//         response.json("User's last name was updated!");
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// })
+
+// //update an user's description
+// app.put('/users/:user_id', async (request, response) =>{
+//     try {
+//         const { user_id } = request.params;
+//         const {description} = request.body;
+//         const updateUser = await pool.query(
+//             'UPDATE a_user SET description = $1 WHERE user_id = $2', 
+//             [description, user_id]
+//         );
+//         response.json("User description was updated!");
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// })
+
+// //delete an user
+// app.delete('/users/:user_id', async (request, response) =>{
+//     try {
+//         const { user_id } = request.params;
+//         const deleteUser = await pool.query(
+//             'DELETE FROM a_user WHERE user_id = $1', 
+//             [user_id]
+//         );
+//         response.json("User was deleted!");
+//     } catch (err) {
+//         console.log(err.message);
+//     }
+// })
 
 const getWallet = (request, response) => {
     pool.query('SELECT * FROM a_wallet', (error, results) => {
