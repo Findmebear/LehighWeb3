75daf4fd9d5ced8c66e25bf66d2eb9fc37e0aee7
ziw224 <ziw224@lehigh.edu>
2022-06-30 14:58:21 -0400

    updates on APIs

--- server/database.sql
+++ server/database.sql
@@ -11,3 +11,10 @@ insert into a_user (user_id, first_name, last_name, description) values ('0x10a3
 insert into a_user (user_id, first_name, last_name, description) values ('0xb68653b037f453a34fdf4a932493708e34ed7f80', 'Thorny', 'Prendeguest', 'null');
 insert into a_user (user_id, first_name, last_name, description) values ('0xcae3b8bbda9a317038563455aabe1294ace707d9', 'Carlos', 'Ham', 'null');
 insert into a_user (user_id, first_name, last_name, description) values ('0x10c49efa9ce8d91bdac8ec5e0052f0b1b4fbe833', 'Latrina', 'Paddefield', 'null');
+
+CREATE TABLE a_user_image(
+    image_id SERIAL,
+    name TEXT,
+    img TEXT,
+    CONSTRAINT A_USER_IMAGE_PK PRIMARY KEY(image_id)
+);
--- server/index.js
+++ server/index.js
@@ -23,32 +23,21 @@ const pool = new Pool(
     process.env.NODE_ENV === "production" ? proConfig : devConfig
 );
 
+const fileUpload = require('express-fileupload');
 const port = process.env.PORT || 3000;
 const app = express();
+
 
 app.use(bodyParser.json({limit: "30mb", extend: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extend: true}))
 app.use(cors());
+app.use(fileUpload());
 
 //ROUTES//
 
 app.get('/', (request, response) => {
     response.json({ info: 'Backend Web3 Development Database'})
   });
-
-//create a user
-app.post('/users', async (request, response) =>{
-    try {
-        const { user_id, first_name, last_name, description} = request.body;
-        const newUser = await pool.query(
-            'INSERT INTO a_user (user_id, first_name, last_name, description) VALUES ($1, $2 , $3, $4) RETURNING *',
-             [user_id, first_name, last_name, description]           
-             );
-        response.json(newUser.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
 
 //get all users
 app.get('/users', async (request, response) =>{
@@ -61,9 +50,10 @@ app.get('/users', async (request, response) =>{
 })
 
 //get an user
-app.get('/users/:user_id', async (request, response) =>{
+app.get('/users/:id', async (request, response) =>{
     try {
-        const { user_id } = request.params;
+        // const { user_id } = request.params;
+        const { user_id } = request.body;
         const user = await pool.query(
             'SELECT * FROM a_user WHERE user_id = $1', 
             [user_id]
@@ -74,40 +64,51 @@ app.get('/users/:user_id', async (request, response) =>{
     }
 })
 
-//update an user's first name
-app.put('/users/:user_id', async (request, response) =>{
+//create a user
+app.post('/users', async (request, response) =>{
+    try {
+        const { user_id, first_name, last_name, description} = request.body;
+        const newUser = await pool.query(
+            'INSERT INTO a_user (user_id, first_name, last_name, description) VALUES ($1, $2 , $3, $4) RETURNING *',
+             [user_id, first_name, last_name, description]           
+             );
+        response.json("Successfully inserted new user!");
+    } catch (err) {
+        response.json("User has already existed!");
+    }
+})
+
+//update all the user's information, including first_name, last_name, and description.
+app.put('/users/:id', async (request, response) =>{
     try {
         let body = [];
         request.on('data', (chunk) => {
             body.push(chunk);
         }).on('end', () => {
-            body = Buffer.concat(body).toString();
-        // at this point, `body` has the entire request body stored in it as a string
+            body = Buffer.concat(body).toString();// at this point, `body` has the entire request body stored in it as a string
         });
-        //update first_name
-        const { user_id } = request.params;
+
+        //update user's first_name
+        const { user_id } = request.body;
         const { first_name } = request.body;
         const updateFirstName = await pool.query(
             'UPDATE a_user SET first_name = $1 WHERE user_id = $2', 
             [first_name, user_id]
         );
-        response.json("User's first name was updated!");
 
-        //update last_name
+        //update user's last_name
         const { last_name } = request.body;
         const updateLastName = await pool.query(
             'UPDATE a_user SET last_name = $1 WHERE user_id = $2', 
             [last_name, user_id]
         );
-        response.json("User's last name was updated!");
-
-            // case 3: const {description} = request.body;
-            //         const updateDescription = await pool.query(
-            //             'UPDATE a_user SET description = $1 WHERE user_id = $2', 
-            //             [description, user_id]
-            //         );
-            //         response.json("User description was updated!");  
-            //         break;              
+        //update user's description
+        const {description} = request.body;
+        const updateDescription = await pool.query(
+            'UPDATE a_user SET description = $1 WHERE user_id = $2', 
+            [description, user_id]
+        );     
+        response.json("Successfully updated all the information!");       
         
         
     } catch (err) {
@@ -115,60 +116,19 @@ app.put('/users/:user_id', async (request, response) =>{
     }
 })
 
-// //update an user's last name
-// app.put('/users/:user_id', async (request, response) =>{
-//     try {
-//         const { user_id } = request.params;
-//         const {last_name} = request.body;
-        // const updateUser = await pool.query(
-        //     'UPDATE a_user SET last_name = $1 WHERE user_id = $2', 
-        //     [last_name, user_id]
-        // );
-//         response.json("User's last name was updated!");
-//     } catch (err) {
-//         console.error(err.message);
-//     }
-// })
-
-// //update an user's description
-// app.put('/users/:user_id', async (request, response) =>{
-//     try {
-//         const { user_id } = request.params;
-//         const {description} = request.body;
-//         const updateUser = await pool.query(
-//             'UPDATE a_user SET description = $1 WHERE user_id = $2', 
-//             [description, user_id]
-//         );
-//         response.json("User description was updated!");
-//     } catch (err) {
-//         console.error(err.message);
-//     }
-// })
-
-// //delete an user
-// app.delete('/users/:user_id', async (request, response) =>{
-//     try {
-//         const { user_id } = request.params;
-//         const deleteUser = await pool.query(
-//             'DELETE FROM a_user WHERE user_id = $1', 
-//             [user_id]
-//         );
-//         response.json("User was deleted!");
-//     } catch (err) {
-//         console.log(err.message);
-//     }
-// })
-
-const getWallet = (request, response) => {
-    pool.query('SELECT * FROM a_wallet', (error, results) => {
-        if(error){
-            throw error
-        }
-        return response.status(200).json(results.rows)
-    })
-}  
-
-app.get('/balance/:wallet_id', getWallet)
+//delete an user
+app.delete('/users/:id', async (request, response) =>{
+    try {
+        const { user_id } = request.body;
+        const deleteUser = await pool.query(
+            'DELETE FROM a_user WHERE user_id = $1', 
+            [user_id]
+        );
+        response.json("User was deleted!");
+    } catch (err) {
+        response.json("User not exists!");
+    }
+})
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
