f0922afee6259e9930fe750448844302cfbf88ba
Findmebear <arm320@lehigh.edu>
2022-07-01 20:09:17 -0400

    modifying smart contract + utils

--- contracts/CreatePost.sol
+++ contracts/CreatePost.sol
@@ -5,12 +5,9 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";   //perform console logs provided by hardhat
 
 contract CreatePost {
-  uint public postCount = 0;    //only positive values
-  string public name = "CreatePost";
-  //Mapping is a reference type as arrays and structs
-  mapping(uint => Post) public posts;
+  uint postCount = 0;  //only positive values
 
-  struct Post {
+  struct PostStruct {
     uint id;
     string text;
     string hash;
@@ -26,9 +23,7 @@ contract CreatePost {
     address author
   );
 
-  constructor(){
-    
-  }
+  PostStruct[] posts;
 
   function createPost(string memory _videoHash, string memory _text, string memory _title) public {
     // Make sure the text exists and smaller than 500 
@@ -38,14 +33,21 @@ contract CreatePost {
     // Make sure video title exists
     require(bytes(_title).length > 0);
     // Make sure uploader address exists
-    require(msg.sender!=address(0));
+    require(msg.sender != address(0));
 
     // Increment video id
     postCount++;
-
-    // Add video to the contract
-    posts[postCount] = Post(postCount, _text, _videoHash, _title, msg.sender);
+    posts.push(PostStruct(postCount, _text, _videoHash, _title, msg.sender));
     // Trigger an event
     emit PostCreated(postCount, _text, _videoHash, _title, msg.sender); 
+  }
+  //returns the current postCount
+  function getPostCount() public view returns (uint) {
+    return postCount;
+  }
+
+  //returns the map of posts
+  function getAllPosts() public view returns (PostStruct[] memory) {
+    return posts;
   }
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 import abi from './CreatePost.json'
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x70A0097326A25208E0Bf249aeF914897CB2F28E1';
+export const contractAddress = '0xF9DE189274e5d227D73fB0005b729a5DB2d8453a';
 
