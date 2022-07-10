1636f0af3c8c68c243d2d5b4327deab2f4b28836
Steven Nazaroff <stn223@lehigh.edu>
2022-07-10 18:36:10 -0400

    created button that once you click can get user information of user

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,53 +1,70 @@
-import React from 'react';
-import axios from 'axios';
+import React, {useState} from 'react';
+import Axios from 'axios';
 import "./profile.css";
 
-export default class Profile extends React.Component {
+function Profile(){
+    const [person, setPerson] = useState("");
 
-    //code that gets the name of users based on their id
-    state = {
-        persons: [],
-    }
+    const getInfo = () => {
+        fetch('http://localhost:3000/users/0x10a387cd1641775244cbc2ae2b48710dc80e4fab')
+        .then((response) => response.json())
+        .then((data) => {
+            setPerson(data.first_name + "..." + data.last_name);
 
-    componentDidMount() {
-        axios.get('http://localhost:3000/users')
-        .then(res => {
-            const persons = res.data;
-            this.setState({ persons })
-            console.log(persons);
-        })
+        });
 
-        axios.get("http://localhost:3000/user/:user_id", {
-            params:{
-                user_id: "0xb68653b037f453a34fdf4a932493708e34ed7f80"
-            }
-        })
-            .then(function (response) {
-                return response.data;
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            })
-    }
-    render() {
-        return (
-            <ul>
-                <div class="card" >
-                    <p class="title">{
-                        this.state.persons
-                            .map(person =>
-                                <li key={person.user_id}>{person.first_name} {person.last_name}</li>
-                            )
-                    } </p>
-
-                </div>
-            </ul>
-        )
-    }
+    };
+    return(
+        <div>
+            <button onClick={getInfo}>GetPersonInfo</button>
+            {person}
+        </div>
+    );
 }
+export default Profile;
+
+// import React from 'react';
+// import axios from 'axios';
+// import "./profile.css";
+
+// export default class Profile extends React.Component {
+
+//     //code that gets the name of users based on their id
+//     state = {
+//         persons: [],
+//     }
+
+//     componentDidMount() {
+//         axios.get('http://localhost:3000/users', {
+//             param: {
+//                 "user_id": "0x10a387cd1641775244cbc2ae2b48710dc80e4fab"
+//             }
+//         })
+//             .then(res => {
+//                 const persons = res.data;
+//                 this.setState({ persons })
+//                 console.log(persons.user_id);
+//             })
+//     }
+
+//     render() {
+//         return (
+//             <ul>
+//                 {
+//                     <div class="card" >
+//                         <p class="title">{
+//                             this.state.persons
+//                                 .map(person =>
+//                                     <li key={person.user_id}>{person.first_name} {person.last_name}</li>
+//                                 )
+//                         } </p>
+
+//                     </div>
+//                 }
+//             </ul>
+//         )
+//     }
+// }
 
 //https://jsonplaceholder.typicode.com/users
 //https://stackoverflow.com/questions/64312119/send-axios-api-requests-from-react-app-deployed-on-heroku
@@ -60,3 +77,6 @@ export default class Profile extends React.Component {
     <li key = {person.user_id}>{person.name}</li>
 ))}
 </ul> */}
+
+
+//https://www.youtube.com/watch?v=rpg1jOvGCtQ
