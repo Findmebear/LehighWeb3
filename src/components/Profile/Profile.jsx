77cb0189b72672303aacb375fde7ad3af520231e
Steven Nazaroff <stn223@lehigh.edu>
2022-07-11 13:46:43 -0400

    deleted comments

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -19,12 +19,6 @@ function Profile() {
             }
         );
 
-        // .then((response) => response.json())
-        // .then((data) => {
-        //     setPerson(data.first_name + " " + data.last_name +
-        //         "\n" + data.description + " " + data.user_id);
-        // });
-
     };
     return (
         <div class="card">
@@ -39,48 +33,6 @@ function Profile() {
 }
 export default Profile;
 
-// import React from 'react';
-// import axios from 'axios';
-// import "./profile.css";
-
-// export default class Profile extends React.Component {
-
-//     //code that gets the name of users based on their id
-//     state = {
-//         persons: [],
-//     }
-
-//     componentDidMount() {
-//         axios.get('http://localhost:3000/users', {
-//             param: {
-//                 "user_id": "0x10a387cd1641775244cbc2ae2b48710dc80e4fab"
-//             }
-//         })
-//             .then(res => {
-//                 const persons = res.data;
-//                 this.setState({ persons })
-//                 console.log(persons.user_id);
-//             })
-//     }
-
-//     render() {
-//         return (
-//             <ul>
-//                 {
-//                     <div class="card" >
-//                         <p class="title">{
-//                             this.state.persons
-//                                 .map(person =>
-//                                     <li key={person.user_id}>{person.first_name} {person.last_name}</li>
-//                                 )
-//                         } </p>
-
-//                     </div>
-//                 }
-//             </ul>
-//         )
-//     }
-// }
 
 //https://jsonplaceholder.typicode.com/users
 //https://stackoverflow.com/questions/64312119/send-axios-api-requests-from-react-app-deployed-on-heroku
